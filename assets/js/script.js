31d9ec1acea6a775c2a1733a6224160827aed797
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-09-09 21:00:56 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// Assignment code here
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+
+var passwordLength;
+
+
+function generatePassword(){
+
+  passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log("Password length " + passwordLength);
+  
+  if(!passwordLength) {
+    alert("Required value");
+
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must choose between 8 and 128");
+    console.log("Password length " + passwordLength);
+  
+
+}
+
+
+
+
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
